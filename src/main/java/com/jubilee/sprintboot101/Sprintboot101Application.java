2fdd913bd4ffec3e38113540cb6c62a34bee0a4f
Jubilee Muthoka <jubilee.muthoka@gmail.com>
2020-03-02 21:17:34 -0500

    Initial commit

---
+++ src/main/java/com/jubilee/sprintboot101/Sprintboot101Application.java
@@ -0,0 +1,13 @@
+package com.jubilee.sprintboot101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprintboot101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sprintboot101Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jubilee/sprintboot101/Sprintboot101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jubilee.sprintboot101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprintboot101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
